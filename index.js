2adeb01cd6c200ff96c50eb62c975fe65d2ab0cf
ahmed <ahmed.elghoul@infor.com>
2020-05-03 14:04:51 +0100

    price tracker demo

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+
+const sendNotification = require('./sendMail')
+
+const url = 'https://www.mytek.tn/climatiseur-tunisie/11475-climeur-mobile-coala-7l.html';
 
 (async () => {
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/google-chrome-stable',
     })
-    const page = await browser.newPage();
-    await page.goto('');
 
-    
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const content = await page.content();
+    const $ = cheerio.load(content);
+
+
+    $('#old_price_display').each(function (index, ele) {
+        if (!!$(ele).text()) {
+            const price = $('#our_price_display').text();
+            const cprice = price.replace(/[^0-9.-]/g, "")
+            if (+cprice < 259000) { // 259 dinar 😃
+                sendNotification(cprice, url);
+            }
+        }
+    })
 
     await browser.close();
 })();
+
+
+
+
+
+
---
+++ sendMail.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer');
+
+module.exports = async (price, url) => {
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_FROM,
+            pass: process.env.GMAIL_PASSWORD
+        }
+    });
+
+
+    let info = await transporter.sendMail({
+        from: '"Price Notify" <*****@gmail.com>',
+        to: process.env.GMAIL_TO,
+        subject: 'Price dropped to ' + price,
+        text: 'Price dropped to ' + price,
+        html: `<a href=\"${url}\">Link</a>`
+    });
+
+    console.log(info.messageId);
+}
